0517679433f511f93da1e03740b4a92634c15598
lijanahmmed <lijanahmmed@gmail.com>
2025-11-13 19:36:59 +0600

    added vercel.json for deployment

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("FinEase_DB");
     const transactionCollection = db.collection("transaction");
@@ -108,7 +108,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
